6450958d63468340665a0aedb5f5ddba84ec7be4
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-29 22:00:12 +0100

    finish

--- TemperatureConverter/TemperatureConverter/MainWindow.xaml.cs
+++ TemperatureConverter/TemperatureConverter/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,26 +22,72 @@ namespace TemperatureConverter
     /// </summary>
     public partial class MainWindow : Window
     {
-        EnteringTemperature entering = new EnteringTemperature();
+        
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void CelciusButton_Click(object sender, RoutedEventArgs e)
+      
+        public double number;
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
         {
-            entering.InitializeComponent();
+            
+
+            
+            if (FahrenheitText.Text != "Fahrenheit")
+            {
+                Fahrenheit();
+            }
+            else if (CelciusText.Text != "Celcius")
+            {
+                Celcius();
+            }
+            else if (KelvinText.Text != "Kelvin")
+            {
+                Kelvin();
+            }
+            else
+            {
+                KelvinText.Text = "ERROR";
+                CelciusText.Text = "ERROR";
+                FahrenheitText.Text = "ERROR";
+            }
         }
 
-        private void KelvinButton_Click(object sender, RoutedEventArgs e)
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
-
+            KelvinText.Text = "Kelvin";
+            CelciusText.Text = "Celcius";
+            FahrenheitText.Text = "Fahrenheit";
         }
 
-        private void FahrenheitButton_Click(object sender, RoutedEventArgs e)
-        {
+        
 
+        public int Kelvin()
+        {
+            number = double.Parse(KelvinText.Text);
+            CelciusText.Text = (number - 273).ToString();
+            FahrenheitText.Text = ((number * 1.8) - 459.67).ToString();
+            return 0;
+        }
+
+        public int Fahrenheit()
+        {
+            float i = 5 / 9;
+            number = float.Parse(FahrenheitText.Text);
+            CelciusText.Text = ((number - 32) / 1.8).ToString();
+            KelvinText.Text = ((number + 460) * i).ToString();
+            return 0;
+        }
+        public int Celcius()
+        {
+            number = double.Parse(CelciusText.Text);
+            FahrenheitText.Text = (32 + 1.8 * number).ToString(); ;
+            KelvinText.Text = (number + 273).ToString();
+            return 0;
         }
     }
 }
